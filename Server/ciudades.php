186a1733a18e2fa66b2a65e9cd5cfe52733a3254
isaacpizarro95 <40573933+isaacpizarro95@users.noreply.github.com>
2019-11-08 00:50:47 +0100

    Add files via upload

---
+++ Server/ciudades.php
@@ -0,0 +1,13 @@
+<?php
+    $respuesta = new stdClass();
+
+    require_once('conexion_server.php');
+    $new_conexion = new ConexionBD();
+    $conex = $new_conexion->getConex();
+    $resultado = $conex->query("SELECT DISTINCT `ciudad` FROM `piso`");
+    $temp = $resultado->fetch_all();
+    for($i = 0; $i < sizeof($temp); $i++){
+       $respuesta->ciudades[$i] = $temp[$i][0]; 
+    }
+    print(json_encode($respuesta));
+?>
