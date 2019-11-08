1b4d3265b45eca4321c88876927e9c1d373103a6
RafaPC <you@example.com>
2019-11-08 01:43:23 +0100

    Función de login ya funciona

--- Server/login.php
+++ Server/login.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: application/json');
 if(isset($_POST['login']) && isset($_POST['password'])){
     require_once('conexion_server.php');
     $new_conexion = new ConexionBD();
@@ -8,13 +9,13 @@ if(isset($_POST['login']) && isset($_POST['password'])){
     $resultado = $conex->query("SELECT * FROM `login_password` WHERE `login` ='$login' AND `password` = '$password'");
     $temp = $resultado->fetch_all();
     if (!sizeof($temp)){
-        die("{error:-2}");
+        die('{"error":-2}');
     }
     else{
-        die("{error: 1}");
+        die('{"error": 1}');
     }
 }else{
-    die("{error:-1}");
+    die('{"error":-1}');
 }
 $conex->close();
 
