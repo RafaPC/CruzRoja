f9ebb2abbef6b664ffbe649d47c6dfd0d521dd29
isaacpizarro95 <isaacpizarro1995@gmail.com>
2019-11-08 02:12:00 +0100

    faltan cosas

--- Server/login.php
+++ Server/login.php
@@ -6,13 +6,13 @@ if(isset($_POST['login']) && isset($_POST['password'])){
     $conex = $new_conexion->getConex();
     $login = $_POST['login'];
     $password = $_POST['password'];
-    $resultado = $conex->query("SELECT * FROM `login_password` WHERE `login` ='$login' AND `password` = '$password'");
+    $resultado = $conex->query("SELECT `tipo` FROM `login_password` WHERE `login` ='$login' AND `password` = '$password'");
     $temp = $resultado->fetch_all();
     if (!sizeof($temp)){
         die('{"error":-2}');
     }
     else{
-        die('{"error": 1}');
+        die('{"tipo":'.$temp[0][0].'}');
     }
 }else{
     die('{"error":-1}');
