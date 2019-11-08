0fc91ce655b31cecd6cd24288d0f88b56a1d2c60
isaacpizarro95 <isaacpizarro1995@gmail.com>
2019-11-08 08:12:17 +0100

    Todo hecho

--- Server/modificaciones_vol.php
+++ Server/modificaciones_vol.php
@@ -5,19 +5,19 @@ if(isset($_GET['id'])){
     $new_conexion = new ConexionBD();
     $conex = $new_conexion->getConex();
     $id = $_GET['id'];
-    $num_usuarios = $GET['num_usuarios'];
-    $num_voluntarios = $GET['num_voluntarios'];
-    $horario = $GET['horario'];
-    $lavadora = $GET['lavadora'];
-    $plancha = $GET['plancha'];
-    $ducha = $GET['ducha'];
-    $cocina = $GET['cocina'];
-    $comida = $GET['comida'];
-    $accesibilidad = $GET['accesibilidad']
+    $num_usuarios = $_GET['num_usuarios'];
+    $num_voluntarios = $_GET['num_voluntarios'];
+    $horario = $_GET['horario'];
+    $lavadora = $_GET['lavadora'];
+    $plancha = $_GET['plancha'];
+    $ducha = $_GET['ducha'];
+    $cocina = $_GET['cocina'];
+    $comida = $_GET['comida'];
+    $accesibilidad = $_GET['accesibilidad'];
     $resultado = $conex->query("SELECT * FROM `piso` WHERE `id` ='$id'");
     $sql = "UPDATE `piso` SET `num_usuarios`= $num_usuarios, `num_voluntarios`= $num_voluntarios, `horario` = $horario, `lavadora` = $lavadora, 
     `plancha` = $plancha, `ducha` = $ducha, `cocina` = $cocina, `comida` = $comida, `accesibilidad` = $accesibilidad WHERE `id` = $id";
-    if (($conexion->query($sql))){
+    if ($sql){
         die("{error: 1}");
     }
     else{
