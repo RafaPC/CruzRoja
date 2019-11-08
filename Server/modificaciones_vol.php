9f1629af2b5f5a6227aad8f6e0252bc703ea0d3e
isaacpizarro95 <isaacpizarro1995@gmail.com>
2019-11-08 07:40:32 +0100

    ahora con actualizaciones de voluntario

---
+++ Server/modificaciones_vol.php
@@ -0,0 +1,29 @@
+<?php
+
+if(isset($_GET['id'])){
+    require_once('conexion_server.php');
+    $new_conexion = new ConexionBD();
+    $conex = $new_conexion->getConex();
+    $id = $_GET['id'];
+    $num_usuarios = $GET['num_usuarios'];
+    $num_voluntarios = $GET['num_voluntarios'];
+    $horario = $GET['horario'];
+    $lavadora = $GET['lavadora'];
+    $plancha = $GET['plancha'];
+    $ducha = $GET['ducha'];
+    $cocina = $GET['cocina'];
+    $comida = $GET['comida'];
+    $resultado = $conex->query("SELECT * FROM `piso` WHERE `id` ='$id'");
+    $sql = "UPDATE `piso` SET `num_usuarios`= $num_usuarios, `num_voluntarios`= $num_voluntarios, `horario` = $horario, `lavadora` = $lavadora, 
+    `plancha` = $plancha, `ducha` = $ducha, `cocina` = $cocina, `comida` = $comida WHERE `id` = $id";
+    if (($conexion->query($sql))){
+        die("{error: 1}");
+    }
+    else{
+        die("{error: -2}");
+    }
+}else{
+    die("{error:-1}");
+}
+
+?>
